d84013dcb4bffd86b742650a53d94af9a41fec16
Gede Rickelme Dewantara <rickelme@Gedes-MacBook-Pro.local>
2026-03-10 23:49:37 +0700

    chore: remove luxury yachts and placeholder empty clients from the trusted by marquee

--- src/db/clients.ts
+++ src/db/clients.ts
@@ -2,24 +2,6 @@ import { Client } from "@/types/videos";
 
 export const clientsData: Client[] = [
   {
-    id: "decluttr",
-    name: "Decluttr",
-    logo: "/companies/decluttr.png",
-    hidden: true,
-  },
-  {
-    id: "ichiban",
-    name: "Ichiban",
-    logo: "/companies/ichiban.png",
-    hidden: true,
-  },
-  {
-    id: "luxury-yachts-co",
-    name: "Luxury Yachts Co",
-    logo: "/companies/luxury-yachts-co.png",
-    hidden: true,
-  },
-  {
     id: "nft-co",
     name: "NFT.co / Docial",
     logo: "/companies/nft-docial.png",
@@ -45,43 +27,8 @@ export const clientsData: Client[] = [
     logo: "/companies/importir-org.png",
   },
   {
-    id: "fam-estate",
-    name: "FAM Estate",
-    logo: "/companies/placeholder-client.png",
-  },
-  {
-    id: "chef-elin-sulivan",
-    name: "Chef Elin Sulivan",
-    logo: "/companies/placeholder-client.png",
-  },
-  {
     id: "intrapreneur",
     name: "Intrapreneur",
     logo: "/companies/entrepreneurs-id.png",
-  },
-  {
-    id: "joseph",
-    name: "Joseph",
-    logo: "/companies/placeholder-client.png",
-  },
-  {
-    id: "vacuvin",
-    name: "VacuVin",
-    logo: "/companies/placeholder-client.png",
-  },
-  {
-    id: "jean-yip",
-    name: "Jean Yip",
-    logo: "/companies/placeholder-client.png",
-  },
-  {
-    id: "personal-project",
-    name: "Personal Project",
-    logo: "/companies/placeholder-client.png",
-  },
-  {
-    id: "good-laundry",
-    name: "Good Laundry",
-    logo: "/companies/placeholder-client.png",
   },
 ];
